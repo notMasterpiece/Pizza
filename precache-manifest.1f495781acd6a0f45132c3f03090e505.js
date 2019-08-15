b92e10fe60d30ac30838688d6be16159f75ae00c
Administrator <administrator@iMac.local>
2019-08-15 14:33:25 +0300

    Updates

---
+++ precache-manifest.1f495781acd6a0f45132c3f03090e505.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b374a75732629b583624eae5ff378a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b51be9150c3d63eaa3a",
+    "url": "/static/css/main.c49e42cb.chunk.css"
+  },
+  {
+    "revision": "269f9428e949666b7095",
+    "url": "/static/js/2.21e0b6d1.chunk.js"
+  },
+  {
+    "revision": "1b51be9150c3d63eaa3a",
+    "url": "/static/js/main.8eb2a66f.chunk.js"
+  },
+  {
+    "revision": "7a518d946e6be2e6f42e",
+    "url": "/static/js/runtime~main.73cc5a00.js"
+  },
+  {
+    "revision": "03cf39bdc2b923dfecc13ba02a604cc3",
+    "url": "/static/media/close.03cf39bd.svg"
+  },
+  {
+    "revision": "26246eed132bb470bafc7b2eb4a3ec07",
+    "url": "/static/media/constractor_pizza.26246eed.png"
+  },
+  {
+    "revision": "4a1ae384e470595d722f46614a4e5afb",
+    "url": "/static/media/ingredient_type_cheese.4a1ae384.png"
+  },
+  {
+    "revision": "0ee12be7b95ad7187b336e80beaadc5b",
+    "url": "/static/media/ingredient_type_fish.0ee12be7.png"
+  },
+  {
+    "revision": "b15b205a7215e0eb05734f0c8157e5c0",
+    "url": "/static/media/ingredient_type_meat.b15b205a.png"
+  },
+  {
+    "revision": "9d63633451a4500f8df367c6e2027666",
+    "url": "/static/media/ingredient_type_vegetable.9d636334.png"
+  },
+  {
+    "revision": "3907ae7917f3064df1aa8f67bf419804",
+    "url": "/static/media/pepperoni_salami.3907ae79.png"
+  }
+]);
